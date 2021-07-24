c27655b7b61d77255cb222210b662ff8fe67b5ee
ayanda <64249187+ayandagatsha@users.noreply.github.com>
2021-07-24 12:09:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+  document.body.className += " loaded";
+}
+
+let div = document.createElement("div");   
+            
+document.body.appendChild(div);               
+div.id="container"
+div.style=`background-image: url("background_image.jpg")`;
+
+
+
